9da95ec6e82ccd723084d8ec2436e497e02027a5
lauracucalon <lauracucaloncastilla@gmail.com>
2023-05-29 18:26:22 +0200

    uptodate

--- GETNEXTLINE/get_next_line.c
+++ GETNEXTLINE/get_next_line.c
@@ -6,9 +6,59 @@
 /*   By: lacucalo <lacucalo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 11:44:09 by lacucalo          #+#    #+#             */
-/*   Updated: 2023/05/29 14:40:47 by lacucalo         ###   ########.fr       */
+/*   Updated: 2023/05/29 18:23:17 by lacucalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char *new_line(char *saved)
+{
+    int i;
+    char *line;
+    
+    i = 0;
+    while(saved[i] != '\n' && saved[i] != '\0') //strlen
+        i++;
+    if (saved[i] == '\n')
+        i++;
+    line = malloc(sizeof(char) * i + 1);
+    //Control de errores
+    i = 0;
+    while(saved[i] != '\n' && saved[i] != '\0')
+    {
+        line[i] = saved[i];
+        i++;
+    }
+    if(saved[i] == '\n')
+        line[i++] = '\n';
+    line[i] = '\0';
+    return(line);
+}
+
+char *get_next_line(int fd)
+{
+    static char *saved;
+    char *line;
+    ssize_t countbytes;
+    //char *temp;
+
+    if(fd < 0 || BUFFER_SIZE <= 0)
+        return(NULL);
+    countbytes = read(fd, saved, BUFFER_SIZE);   //si falla probar con temp
+
+    line = new_line(saved);
+    //saved = del_line(saved);
+    //printf("%s", saved);
+    printf("%s\n", "error");
+    return(line);
+}
+
+int main(void)
+{
+    int fd;
+    
+    fd = open("text.txt", O_RDONLY);
+    char *line = get_next_line(fd);
+    printf("%s", line);
+}
--- GETNEXTLINE/get_next_line.h
+++ GETNEXTLINE/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lacucalo <lacucalo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 17:05:11 by lacucalo          #+#    #+#             */
-/*   Updated: 2023/05/26 17:07:03 by lacucalo         ###   ########.fr       */
+/*   Updated: 2023/05/29 17:16:18 by lacucalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,15 @@
 
 # endif
 
-#include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h> 
+# include <unistd.h>
+# include <stdarg.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
 
-//FUNCIONES
+char *get_next_line(int fd);
 
 #endif
