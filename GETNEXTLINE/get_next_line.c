d1d8bb5eeb734b5732ea4a6f2effd811b437dfcd
lauracucalon <lauracucaloncastilla@gmail.com>
2023-05-29 15:50:24 +0200

    uptodate

--- GETNEXTLINE/get_next_line.c
+++ GETNEXTLINE/get_next_line.c
@@ -6,7 +6,9 @@
 /*   By: lacucalo <lacucalo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 11:44:09 by lacucalo          #+#    #+#             */
-/*   Updated: 2023/05/29 11:44:10 by lacucalo         ###   ########.fr       */
+/*   Updated: 2023/05/29 14:40:47 by lacucalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
 
